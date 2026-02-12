aa76f7598a52427f8b64b6254ffd9bd82161241f
Shakeel25-tech <sahils20357@gmail.com>
2026-02-12 16:42:48 +0000

    var and datatype config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "abc@gmail.com"
+var accountPassword = "12345"
+accountCity = "Lucknow"
+
+// accountId = 2
+
+accountEmail = "sahil@gmail.com"
+accountPassword = "112233"
+accountCity = "Bengaluru"
+console.table([accountEmail, accountPassword, accountCity]);
+
+/* 
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,9 @@
+//number => 2 to the ppower 53
+//bigint
+//string = ""
+//boolean => true/false
+//undefined =>
+//symbol =>unique
+//object
+
+console.log(typeof "Shakeel");
