95e2e1d8c6bff748d7cdf5f2875e41e98093a02b
Shakeel25-tech <sahils20357@gmail.com>
2026-02-14 12:55:42 +0000

    operation done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -12,3 +12,20 @@ console.log(typeof valueInNumber);
 // true => 1; false =>0
 
 
+
+//***************************** Operations ***************************
+
+
+
+let str1 = "Hii"
+let str2 = " Shakeel"
+console.log(str1+str2);
+
+
+console.log(1+2);//normal 3
+console.log(1+"2");//12 concatenation perform
+console.log(1+2+"2");//firstly add and make 3 then concatenate with 2 so this will 32
+console.log("1"+2+2);//concalenate all and make 122
+
+
+
