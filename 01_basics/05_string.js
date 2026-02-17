7375cbe08da3b4b8ad6655c1427a8f5e1351f344
Shakeel25-tech <sahils20357@gmail.com>
2026-02-17 16:36:08 +0000

    string update

---
+++ 01_basics/05_string.js
@@ -0,0 +1,9 @@
+const name = "shakeel"
+const repoCount = 10
+// console.log(name+" "+repoCount);
+
+
+console.log(`Hello my name is ${name} and repoCount is ${repoCount}`);
+
+const gameName = new String('sahil')
+console.log(gameName);
