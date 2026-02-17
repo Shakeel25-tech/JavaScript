32763b789cb7ad044aab9066de83da51d9c31dc1
Shakeel25-tech <sahils20357@gmail.com>
2026-02-17 16:50:19 +0000

    string config

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -6,4 +6,10 @@ const repoCount = 10
 console.log(`Hello my name is ${name} and repoCount is ${repoCount}`);
 
 const gameName = new String('sahil')
-console.log(gameName);
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('a'));
+console.log(gameName.substring(0,4));
+console.log(gameName.slice(-5,3));
+
+
+
