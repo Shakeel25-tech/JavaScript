8c8011b4d0f1555c419accf84df0b5e9d6ceb376
Shakeel25-tech <sahils20357@gmail.com>
2026-02-18 18:23:13 +0000

    update

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -7,6 +7,7 @@ console.log(`Hello my name is ${name} and repoCount is ${repoCount}`);
 
 const gameName = new String('sahil')
 console.log(gameName.charAt(2));
+console.log(gameName.charAt(3));
 console.log(gameName.indexOf('a'));
 console.log(gameName.substring(0,4));
 console.log(gameName.slice(-5,3));
