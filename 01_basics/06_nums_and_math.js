f72167b961ef5126e27323cc425031f7956f4555
Shakeel25-tech <sahils20357@gmail.com>
2026-02-19 08:08:12 +0000

    nums and math done

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -37,5 +37,22 @@ console.log(hundreds.toLocaleString('en-IN'));
 
 //+++++++++++++++++++++++ Maths +++++++++++++++++++++++++
 
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.5));
+console.log(Math.floor(4.5));
+console.log(Math.ceil(4.5));
+console.log(Math.min(1,2,34,5));
+console.log(Math.max(1,2,34,5));
 
+//Understanding random method
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min + 1))+min);
 
