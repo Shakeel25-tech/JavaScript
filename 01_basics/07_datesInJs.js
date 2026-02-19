774e2641070b4ee16a1572c9ccfc2d27170d1f92
Shakeel25-tech <sahils20357@gmail.com>
2026-02-19 10:22:43 +0000

    dates config done

---
+++ 01_basics/07_datesInJs.js
@@ -0,0 +1,18 @@
+//+++++++++++++++++++++ Dates +++++++++++++++++++++++
+
+let myDate = new Date();
+console.log(typeof myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+
+
+//create ur own date
+
+const myCreatedDate = new Date(2026,1,19)
+console.log(myCreatedDate.toLocaleString());
+
+
+
+
+
