8118ae24df1a7262dfdf4000ff6d515caf4bf578
Shakeel25-tech <sahils20357@gmail.com>
2026-02-21 18:07:44 +0000

    change in array02

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -1,9 +1,14 @@
 const marble_heros = ["Ironman", "Thor", "Spiderman"];
 const dc_heros = ["Flash", "Superman", "Batman"];
 
-marble_heros.push(dc_heros);
+// marble_heros.push(dc_heros);
 
-console.log(marble_heros);
+// console.log(marble_heros);
 
-console.log(marble_heros[3][1]);
+// console.log(marble_heros[3][1]);
+
+const all_heros = marble_heros.concat(dc_heros);
+console.log(all_heros);
+
+
 
