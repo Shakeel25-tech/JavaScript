76c4cbbf42d3c979734b14aaf03f34f8a8910acf
Shakeel25-tech <sahils20357@gmail.com>
2026-02-21 17:52:11 +0000

    update in array

---
+++ 02_basics/02_array.js
@@ -0,0 +1,9 @@
+const marble_heros = ["Ironman", "Thor", "Spiderman"];
+const dc_heros = ["Flash", "Superman", "Batman"];
+
+marble_heros.push(dc_heros);
+
+console.log(marble_heros);
+
+console.log(marble_heros[3][1]);
+
