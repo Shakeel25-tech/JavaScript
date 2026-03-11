8538232ac640b2c47ee5d3d6e8c7a7d0b65ad8f4
Shakeel25-tech <sahils20357@gmail.com>
2026-03-11 04:51:32 +0000

    array updated

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -1,14 +1,35 @@
-const marble_heros = ["Ironman", "Thor", "Spiderman"];
+const marvel_heros = ["Ironman", "Thor", "Spiderman"];
 const dc_heros = ["Flash", "Superman", "Batman"];
 
 // marble_heros.push(dc_heros);
 
-// console.log(marble_heros);
+// console.log(marvel_heros);
 
-// console.log(marble_heros[3][1]);
+// console.log(marvel_heros[3][1]);
 
-const all_heros = marble_heros.concat(dc_heros);
+
+//concat operator
+const all_heros = marvel_heros.concat(dc_heros);
 console.log(all_heros);
+
+//spread operator
+const all_new_heros = [...marvel_heros, ...dc_heros]
+console.log(all_new_heros);
+
+//Array.from() → convert something into array
+console.log(Array.isArray("Shakeel"));//return false because shakeel is not a array
+console.log(Array.from("Shakeel"));//return shakeel in an array form
+console.log(Array.from({name:"Shakeel"}))//it will return an empty array untill u specify key or value to change in array
+
+//Array.of() → create array from values
+let score1 = 100
+let score2 = 200
+let score3 = 300
+console.log(Array.of(score1,score2,score3));
+
+
+
+
 
 
 
