53b60aca7efc5fdaf7a8d3c84c10d2ef0290ee8b
Shakeel25-tech <sahils20357@gmail.com>
2026-03-24 17:22:39 +0000

    update object

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,15 @@
+// Singleton
+//Object.create
+
+//object literals 
+const JsUser = {
+    name: "Shakeel",
+    age: 18,
+    location: "Lucknow",
+    email: "Shakeel@gmail.com",
+    isLoggedIn: false,
+    LastLoginDays: ["Monday", "Saturday"]
+
+}
+console.log(JsUser.email);//way of accessing the element
+console.log(JsUser["email"]);//another way of accessing element
