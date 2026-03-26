f42eac214d38ff24f32139fc8a4ce5d40b739622
Shakeel25-tech <sahils20357@gmail.com>
2026-03-26 05:54:14 +0000

    function basic

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -13,3 +13,13 @@ function loginUserMessage(username="sam"){
     
 }
 console.log(loginUserMessage("Raj"));
+
+
+function calculateCartPrize(val1,val2,...num1){
+    return num1
+}
+console.log(calculateCartPrize(200,300,400,500));
+//function is using the rest parameter syntax (...num1), which collects all remaining arguments into an array.
+//val1 = 200
+//val2 = 300
+//...num1 = [400, 500] (rest of the arguments go into an array)
